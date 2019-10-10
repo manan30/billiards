bdd5b0e0970eba2a4aacef198ab1d81745d13f80
Manan Joshi <mananjoshi1995@gmail.com>
2019-10-10 00:32:52 -0400

    Files cleanup and modifying index.html.

--- src/App.js
+++ src/App.js
@@ -1,7 +1,19 @@
 import React from 'react';
 
+import { Canvas, CodeFAB } from './GlobalStyles';
+import Scene from './views/Scene';
+import Controls from './components/Controls';
+
 function App() {
-  return <div />;
+  return (
+    <>
+      <Canvas>
+        <Scene />
+        <Controls />
+      </Canvas>
+      <CodeFAB />
+    </>
+  );
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,6 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
+import GlobalStyles from './GlobalStyles';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+  <>
+    <GlobalStyles />
+    <App />
+  </>,
+  document.getElementById('root')
+);
