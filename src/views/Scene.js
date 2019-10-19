89b41f7abbc810de287e5c74cc884601d834c8a9
Manan Joshi <mananjoshi1995@gmail.com>
2019-10-19 17:57:09 -0400

    Setting up basic camera

--- src/views/Scene.js
+++ src/views/Scene.js
@@ -19,11 +19,12 @@ function Scene() {
   gl.gammaInput = true;
   gl.gammaOutput = true;
 
-  camera.fov = 23;
+  camera.fov = 45;
   camera.aspect = window.innerWidth / window.innerHeight;
   camera.near = 0.1;
-  camera.far = 100;
+  camera.far = 1000;
 
+  camera.up.set(0, 0, 1);
   camera.position.set(-5, 7, 5);
 
   // const cameraHelper = new THREE.CameraHelper(camera);
