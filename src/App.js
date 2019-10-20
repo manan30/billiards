57f265c574622e6ead8529eb9f3710de6d58fcfc
Manan Joshi <mananjoshi1995@gmail.com>
2019-10-20 15:52:35 -0400

    Making OrbitControls component more customizable and updating dependencies version.

--- src/App.js
+++ src/App.js
@@ -9,7 +9,14 @@ function App() {
     <>
       <Canvas>
         <Scene />
-        <Controls />
+        <Controls
+          enableRotate
+          enablePan={false}
+          maxDistance={100}
+          minDistance={5}
+          minPolarAngle={Math.PI / 6}
+          maxPolarAngle={Math.PI / 2}
+        />
       </Canvas>
       <CodeFAB />
     </>
